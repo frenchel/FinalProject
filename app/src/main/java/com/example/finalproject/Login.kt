a66a9e35d71803cabf95af9b9d019b43910e688c
JanVincentDyogi <janvincentdyogi@gmail.com>
2023-11-29 19:03:32 +0800

    guamgana singup and login

--- app/src/main/java/com/example/finalproject/Login.kt
+++ app/src/main/java/com/example/finalproject/Login.kt
@@ -4,22 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+    private lateinit var dbHelper: DatabaseHandler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        dbHelper = DatabaseHandler(this)
+
         /*LOGIN*/
         val signInButton: Button = findViewById(R.id.btnLogin)
         signInButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
 
+            val email = findViewById<EditText>(R.id.etEmail).text.toString()
+            val password = findViewById<EditText>(R.id.etPassword).text.toString()
+            // Validate if required fields are not empty
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                // Check if the email and password match an existing account
+                val account = dbHelper.getAccountByEmailAndPassword(email, password)
+
+                if (account != null) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Invalid Email or Password", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Email and Password are required", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        /*REGISTER NOW*/
         val registerTextView: TextView = findViewById(R.id.tvRegister)
         registerTextView.setOnClickListener {
             val intent = Intent(this, Register::class.java)
